38a420cb1c8e27d2a273f5693d51b2959a1fab23
bystroff <pes13@yandex.ru>
2023-06-22 10:32:39 +0300

    add tests folder and write some api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,103 @@
 import express, {Request, Response} from 'express'
+import { send } from 'process'
 const app = express()
 const port = 3000
 
+const HTTP_STATUSES = {
+  OK_200: 200,
+  CREATED_201: 201,
+  NO_CONTENT_204: 204,
+
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404
+}
+
+let videos = [
+  {id: 1, title: 'Баста', author: 'Вдудь', canBeDownloaded: false, minAgeRestriction: 18, createdAt: ' ', publicationDate: ' ', availableResolutions: []},
+  {id: 2, title: 'УУУУУУ', author: 'Майкл Наки', canBeDownloaded: false, minAgeRestriction: 1, createdAt: ' ', publicationDate: ' ', availableResolutions: []},
+  {id: 3, title: 'Вечерний выпуск', author: 'Дождь', canBeDownloaded: false, minAgeRestriction: 15, createdAt: ' ', publicationDate: ' ', availableResolutions: []},
+  {id: 4, title: 'Путь самурая', author: 'IT-KAMASUTRA', canBeDownloaded: false, minAgeRestriction: 5, createdAt: ' ', publicationDate: ' ', availableResolutions: []},
+  {id: 5, title: 'Реальное собеседование Front-end', author: 'Ulbi TV', canBeDownloaded: false, minAgeRestriction: 6, createdAt: ' ', publicationDate: ' ', availableResolutions: []},
+  {id: 6, title: 'Урок 1 - Типы данных и их модификаторы', author: 'CSTDIO', canBeDownloaded: false, minAgeRestriction: 8, createdAt: ' ', publicationDate: ' ', availableResolutions: []}
+]
+
 app.get('/', (req : Request, res: Response) => {
   res.send('Hello World!')
+})
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+  res.send(204)  
+})
+
+app.get('/videos', (req: Request, res: Response) => {
+  res.send(videos)
+})
+
+app.post('/videos', (req: Request, res: Response) => {
+  let title = req.body.title
+  if (!title || typeof title !== 'string' || !title.trim() || title.length > 40) {
+    res.status(400).send({
+      errorsMessages: [{
+        message: 'Incorrect title',
+        field: 'title'
+      }],
+      resultCode: 1
+    })
+    return
+  }
+
+  const newVideo = {
+    id: +(new Date()),
+    title: title,
+    author: ''
+  }
+  videos.push()
+
+  res.status(201).send(newVideo)
+})
+
+app.get('/videos/:videoId', (req: Request, res: Response) => {
+  const id = +req.params.videoId
+  const video = videos.find(v => v.id === id)
+  if (video) {
+    res.send(video)
+  } else {
+    res.send(404)
+  }
+})
+
+app.put('/videos/:videoId', (req: Request, res: Response) => {
+  let title = req.body.title
+  if (!title || typeof title !== 'string' || !title.trim() || title.length > 40) {
+    res.status(400).send({
+      errorsMessages: [{
+        message: 'Incorrect title',
+        field: 'title'
+      }],
+      resultCode: 1
+    })
+    return
+  }
+
+  const id = +req.params.videoId
+  const video = videos.find(v => v.id === id)
+  if (video) {
+    video.title = title
+    res.status(204).send(video)
+  } else {
+    res.send(404)
+  }
+})
+
+app.delete('/videos/:videoId', (req: Request, res: Response) => {
+  const id = +req.params.videoId;
+  const newVideos = videos.filter(v => v.id !== id)
+  if (newVideos.length < videos.length) {
+    videos = newVideos
+    res.send(204)
+  } else {
+    res.send(404)
+  }
 })
 
 app.listen(port, () => {
